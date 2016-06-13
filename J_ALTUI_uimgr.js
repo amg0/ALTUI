79bfc45cec97e0e5ddedc6de45f5f7e6b88ce955
amg0 <alexis.mermet@gmail.com>
2016-06-13 23:12:59 +0200

    BugFix

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -50,7 +50,7 @@ Status Code:200 OK
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 1733 $";
+var ALTUI_revision = "$Revision: 1734 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -9736,6 +9736,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 				for (var i=0; i<_plugins_data.length; i++) {
 					if (_plugins_data[i].id == id ) {
 						// search version info
+						var plugin = _plugins_data[i]
 						$.each(plugin.Repository, function(i,repo) {
 							if (repo.type == "Vera") {
 								MultiBox.updatePluginVersion("0-0", plugin.id, repo.version, function(result) {
