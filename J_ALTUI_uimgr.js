f40656c9ec15752f8d288daaf0f324c5e022b410
amg0 <alexis.mermet@gmail.com>
2016-06-17 20:16:47 +0200

    bugfix

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -50,7 +50,7 @@ Status Code:200 OK
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 1761 $";
+var ALTUI_revision = "$Revision: 1762 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -9431,7 +9431,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 							type:'input',
 							inputtype:"number",
 							value:v,
-							opt:{required:''}
+							// opt:{required:''}
 							})
 				}
 			})
@@ -9443,7 +9443,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 						type:'input',
 						inputtype:"number",
 						value:v,
-						opt:{required:''}
+						// opt:{required:''}
 						})			
 			});
 			return HTMLUtils.drawFormFields( formfields );
@@ -9474,7 +9474,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 							label:_T(k),
 							type:'input',
 							value:v,
-							opt:{required:''}
+							// opt:{required:''}
 					})
 				})
 				return HTMLUtils.drawFormFields( formfields );
@@ -9510,7 +9510,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 							label:_T(k),
 							type:'input',
 							value:v,
-							opt:{required:''}
+							// opt:{required:''}
 							})
 				}
 			})
@@ -9522,7 +9522,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 						type:'input',
 						// inputtype:"number",
 						value:v,
-						opt:{required:''}
+						// opt:{required:''}
 						})			
 			});
 			return HTMLUtils.drawFormFields( formfields );
