1aac09ce1a38905139772cea392727333ab8ca93
amg0 <alexis.mermet@gmail.com>
2016-06-27 22:14:05 +0200

    Edit button in workflow report page

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -50,7 +50,7 @@ Status Code:200 OK
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 1791 $";
+var ALTUI_revision = "$Revision: 1792 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -8445,6 +8445,11 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 			html +="</ul>";
 			return html;
 		}
+
+		var _toolsWorkflow = [
+			{id:"altui-workflow-edit", glyph:"glyphicon-pencil", label:_T("Edit")},
+		];
+		
 		var workflow = WorkflowManager.getWorkflow(altuiid);
 		if (workflow==null)
 			return;
@@ -8452,11 +8457,21 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 		UIManager.clearPage(_T('Workflow Report'),_T("Workflow Report"),UIManager.oneColumnLayout);
 		var html = "";
 		html += "<div class='col-xs-12'><div id='altui-workflow-report'>";
-		html += _displayWorkflowHeader(workflow)
-		html += _displayWorkflowStats(WorkflowManager.getWorkflowStats(altuiid))
+		html += HTMLUtils.drawToolbar( 'altui-workflow-toolbar', _toolsWorkflow )
+		html += "<div class='panel panel-default'>"
+			html += "<div class='panel-body'>"
+				html += _displayWorkflowHeader(workflow)
+				html += _displayWorkflowStats(WorkflowManager.getWorkflowStats(altuiid))
+			html += "</div>"
+		html += "</div>"
 		html += _displayWorkflowDetails(workflow)
 		html += "</div></div>";
 		$(".altui-mainpanel").append(html);
+		
+		// interactivity
+		$("#altui-workflow-edit").click( function() {
+			_.defer(UIManager.pageWorkflow(workflow.altuiid))
+		})
 	},
 	
 	pageWorkflow: function ( altuiid ) {
