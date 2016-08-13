24b6c20635d6328dbff4f3e559d8f93691998368
amg0 <alexis.mermet@gmail.com>
2016-08-13 18:10:54 +0200

    Fix for network zwave table page

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -50,7 +50,7 @@ Status Code:200 OK
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 1810 $";
+var ALTUI_revision = "$Revision: 1812 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -11819,7 +11819,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 				// console.log("((d.id_parent==razbdevice.id) && (MultiBox.controllerOf(d.altuiid).controller==0)) :%s",
 									// ((d.id_parent==razbdevice.id) && (MultiBox.controllerOf(d.altuiid).controller==0)) )
 				// console.log("result :%s",(d.id_parent==1) || ((d.id_parent==razbdevice.id) && (MultiBox.controllerOf(d.altuiid).controller==0)))
-				return (d.id_parent==1) || ((d.id_parent==razbdevice.id) && (MultiBox.controllerOf(d.altuiid).controller==0))
+				return (d.id_parent==1) || ((razbdevice) && (d.id_parent==razbdevice.id) && (MultiBox.controllerOf(d.altuiid).controller==0))
 			} 
 		);
 		orders = {
