10ae531601b5b435581baf354a14316f486cbe47
amg0 <alexis.mermet@gmail.com>
2016-09-15 22:00:05 +0200

    bugfix for null static_data situation

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -8,7 +8,7 @@
 // written devagreement from amg0 / alexis . mermet @ gmail . com
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 
 /*The MIT License (MIT)
 BOOTGRID: Copyright (c) 2014-2015 Rafael J. Staib
@@ -50,7 +50,7 @@ Status Code:200 OK
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 1818 $";
+var ALTUI_revision = "$Revision: 1822 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -3910,7 +3910,7 @@ var UIManager  = ( function( window, undefined ) {
 			default:
 				var src = defaultIconSrc;
 				var ui_static_data = MultiBox.getDeviceStaticData(device);
-				var str = ui_static_data.default_icon || "";
+				var str = (ui_static_data && ui_static_data.default_icon ) ? ui_static_data.default_icon : "" ;
 				// var dt = _devicetypesDB[ device.device_type ];
 				AltuiDebug.debug("Icon for device altuiid:"+device.altuiid+"  device.type:"+device.device_type);
 				if (ui_static_data!=null)
