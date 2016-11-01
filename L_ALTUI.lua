41f5b7883ca1c10963ba48517008380dab2d7a15
sle118 <sle118@hotmail.com>
2016-11-01 14:29:14 -0400

    Update L_ALTUI.lua

--- L_ALTUI.lua
+++ L_ALTUI.lua
@@ -2992,7 +2992,7 @@ local function _loadDataProviders()
 	debug(string.format("_loadDataProvider()->%s",str) )
 	DataProviders = json.decode(str)
 	for k,v in pairs (DataProviders) do
-		debug(string.format("_loadDataProvider() , DataStorageProviders:%s",json.encode(DataProviders)))
+		debug(string.format("_loadDataProvider() , DataStorageProviders:%s=%s",k,json.encode(v)))
 		if (v["callback"] ~= nil) then
 			local callback_fn = table_search(_G,DataProviders[provider]["callback"],"",0)
 			if callback_fn ~= nil then
