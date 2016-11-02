603a7842850219b54cb7966a231c1c09eb4e26f0
amg0 <alexis.mermet@gmail.com>
2016-11-02 23:19:13 +0100

    bugfix to only display active scenes in timeline view

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -10711,7 +10711,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 							}
 						});
 					}, 
-					null, 
+					function(scene) { return (scene.paused!=1) }, 
 					function(allscenes) {
 						MultiBox.getDevices(
 							function(idx,device) {
