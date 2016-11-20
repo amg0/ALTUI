e895e12f76450fea2752eeef2d30c81ed67f1c1e
amg0 <alexis.mermet@gmail.com>
2016-11-20 16:10:46 +0100

    change if icon in MyHome page

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 1937 $";
+var ALTUI_revision = "$Revision: 1938 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -6281,8 +6281,8 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 						"</div>";
 						
 		var room_toolbar = [
-			{cls:"altui-myhome-onoffdevice btn-lg", glyph:"glyphicon-off", title:_T("Device")},
-			{cls:"altui-myhome-scene btn-lg", glyph:"glyphicon-play", title:_T("Scene")},
+			{cls:"altui-myhome-onoffdevice btn-lg", glyph:"glyphicon-cog", title:_T("Device")},
+			{cls:"altui-myhome-scene btn-lg", glyph:"glyphicon-film", title:_T("Scene")},
 			// {cls:"altui-myhome-sensor", glyph:"glyphicon-flash", title:_T("Sensors")},
 		];
 		// var scene_toolbar = [
