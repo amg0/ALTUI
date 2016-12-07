2780214d66e75e834d27b6e23de750fc842c13d8
amg0 <alexis.mermet@gmail.com>
2016-12-07 18:52:05 +0100

    fix issue with changing an attribute value several time in a row without redisplaying the attribute form

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 1958 $";
+var ALTUI_revision = "$Revision: 1960 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -3759,7 +3759,8 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 									PageMessage.message( "Set Attribute action failed!", "warning" );				
 								}
 								else {
-									PageMessage.message( "Set Attribute succeeded! a LUUP reload will happen now, be patient", "success" );			
+									$(input).attr('value',value)
+									PageMessage.message( "Set Attribute succeeded! a LUUP reload may happen now, be patient", "success" );			
 								}
 							});
 						}
