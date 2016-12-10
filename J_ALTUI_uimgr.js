616845a0ee4a150be2f848628be98838ba579f88
amg0 <alexis.mermet@gmail.com>
2016-12-10 18:43:28 +0100

    add smoke sensors in sensors for page MyHome

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 1960 $";
+var ALTUI_revision = "$Revision: 1961 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -6360,6 +6360,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 			"urn:schemas-micasaverde-com:device:WindowCovering:1"
 		]
 		var sensorfilter = [
+			"urn:schemas-micasaverde-com:device:SmokeSensor:1",
 			"urn:schemas-micasaverde-com:device:LightSensor:1",
 			"urn:schemas-micasaverde-com:device:TemperatureSensor:1",
 			"urn:schemas-micasaverde-com:device:MotionSensor:1",
