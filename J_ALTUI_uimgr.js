dc3a76ed7b9c08eb197d055ae0fc69c93756d39d
amg0 <alexis.mermet@gmail.com>
2017-01-12 21:24:06 +0100

    add door sensor to sensor page of MyHome

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 1985 $";
+var ALTUI_revision = "$Revision: 1987 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -6258,6 +6258,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 		]
 		var sensorfilter = [
 			"urn:schemas-micasaverde-com:device:SmokeSensor:1",
+			"urn:schemas-micasaverde-com:device:DoorSensor:1",
 			"urn:schemas-micasaverde-com:device:LightSensor:1",
 			"urn:schemas-micasaverde-com:device:TemperatureSensor:1",
 			"urn:schemas-micasaverde-com:device:MotionSensor:1",
@@ -9786,7 +9787,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 								yesterday,
 								today
 							)
-						},500);
+						},1000);
 					} else {
 						var today = new Date();
 						var yesterday = new Date();
