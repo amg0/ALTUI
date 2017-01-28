e077b3e3035400b30ca499522ed857d9306aa340
amg0 <alexis.mermet@gmail.com>
2017-01-28 23:01:32 +0100

    support js pattern expression in the device filter

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 2004 $";
+var ALTUI_revision = "$Revision: 2005 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -6688,7 +6688,8 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 		// filter function
 		function deviceFilter(device) {
 			var batteryLevel = MultiBox.getDeviceBatteryLevel(device);
-			var regexp = new RegExp(RegExp.escape(_deviceDisplayFilter.filtername),"i")
+			// var regexp = new RegExp(RegExp.escape(_deviceDisplayFilter.filtername),"i")
+			var regexp = new RegExp(_deviceDisplayFilter.filtername,"i")
 
 			if ((device.invisible == "1") && (_deviceDisplayFilter.invisible != true))
 				return false;
