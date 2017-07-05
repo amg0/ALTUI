c82206a6699fb4270d9a8facd95564d8415383f3
amg0 <alexis.mermet@gmail.com>
2017-07-05 09:42:56 +0200

    bug fix

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 2081 $";
+var ALTUI_revision = "$Revision: 2082 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -7644,7 +7644,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 		];
 		var selected = [];
 		var graph = null;	// will be init later once the scripts are loaded
-
+		var paper = null;
 		function _clearPage() {
 			$(".altui-workflow-toolbar").remove();
 			$(".altui-mainpanel").empty();
@@ -8357,7 +8357,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 			//
 			// Set up Jointjs things
 			//
-			var paper = new joint.dia.Paper({
+			paper = new joint.dia.Paper({
 				el: $('#altui-workflow-canvas'),
 				width: $('#altui-workflow-canvas').parent().innerWidth()-30, height: 600, gridSize: 1,
 				model: graph,
