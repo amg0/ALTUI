05aa3fe9db3b408da9b21c9cffe9f2e4e6142f60
amg0 <alexis.mermet@gmail.com>
2017-09-13 21:46:53 +0200

    bugfix for custom pages with a space in the name (lean mode does not refresh)

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 2099 $";
+var ALTUI_revision = "$Revision: 2100 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -5709,7 +5709,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 		if (str.length != 0)
 			return str
 		str = $(".altui-page-content-one.active").prop("id");
-		return str.substr("altui-page-content-".length);
+		return str.substr("altui-page-content-".length).replace('_',' ');
 		// return $("#altui-page-tabs li.active").text();
 		// return pagename != undefined ? pagename.substring( "altui-page-".length) : '';
 	};
