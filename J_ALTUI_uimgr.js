02b056f164a1c2768395ecf57522607ecda73ff1
amg0 <alexis.mermet@gmail.com>
2017-10-03 23:41:57 +0200

    cosmetics fixes / reneboer

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 2155 $";
+var ALTUI_revision = "$Revision: 2157 $";
 var ALTUI_registered = false;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -577,14 +577,15 @@ var styles ="						\
 		margin-top:5px;			\
 	}\
 	div.altui-battery { \
-		margin-top:2px;			\
-		margin-right:5px;		\
+		margin-top:5px;			\
+		margin-right:2px;		\
 		margin-bottom:0px;		\
 		overflow: visible;		\
 		word-wrap: normal;		\
 	}\
 	div.altui-battery .progress-bar { \
 		color: black;			\
+		border-radius: .25rem;	\
 	}\
 	.caret.caret-reversed {				\
 		border-top-width: 0;			\
