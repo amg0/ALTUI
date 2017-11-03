ffef7aba3868161545f7cf583e3b529adffa4bcc
amg0 <alexis.mermet@gmail.com>
2017-11-03 23:12:20 +0100

    cosmetic

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 2204 $";
+var ALTUI_revision = "$Revision: 2205 $";
 var ALTUI_registered = null;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -5567,7 +5567,7 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 
 	function _createControllerSelect(htmlid) {
 		var html = "";
-		html += "<form class='form-inline col-12'>";
+		html += "<form class='form-inline col-12 mb-2'>";
 			html += "<div class='form-group'>";
 				html += "<label class='col-form-label ' for='altui-controller-select' >"+_T("Controller")+":</label>";
 				html += "<select id='"+htmlid+"' class='form-control'>";
