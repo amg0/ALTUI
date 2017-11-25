567ede92a90ff88548138dcce5b5666d0eed3930
amg0 <alexis.mermet@gmail.com>
2017-11-25 22:55:09 +0100

    fix bug in version ordering of app store

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 2238 $";
+var ALTUI_revision = "$Revision: 2239 $";
 var ALTUI_registered = null;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -9422,9 +9422,9 @@ http://192.168.1.16/port_3480/data_request?id=lu_reload&rand=0.7390809273347259&
 			return arr.sort( function(a,b) {
 					// reverse order
 					if (b.major>a.major)
-						return -1
-					if (b.major<a.major)
 						return 1
+					if (b.major<a.major)
+						return -1
 					return b.minor-a.minor
 				});
 		}
