5e0fa43ca030719cf5c85b2f46b9cf82fd772864
amg0 <alexis.mermet@gmail.com>
2018-04-30 15:45:01 +0200

    version bump

--- L_ALTUI.lua
+++ L_ALTUI.lua
@@ -9,7 +9,7 @@
 local MSG_CLASS = "ALTUI"
 local ALTUI_SERVICE = "urn:upnp-org:serviceId:altui1"
 local devicetype = "urn:schemas-upnp-org:device:altui:1"
-local version = "v2.20b"
+local version = "v2.21"
 local SWVERSION = "3.3.1"	-- "2.2.4"
 local UI7_JSON_FILE= "D_ALTUI_UI7.json"
 local ALTUI_SONOS_MP3 = "altui-sonos.mp3"
