36d9f87704936c9c0454414ddfc2603ef1b2003a
amg0 <alexis.mermet@gmail.com>
2018-05-01 19:48:56 +0200

    bugfix with dialog template and submit buttons

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -38,7 +38,7 @@ THE SOFTWARE.
 // Transparent : //drive.google.com/uc?id=0B6TVdm2A9rnNMkx5M0FsLWk2djg&authuser=0&export=download
 
 // UIManager.loadScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart","table","gauge"]}]}');
-var ALTUI_revision = "$Revision: 2369 $";
+var ALTUI_revision = "$Revision: 2370 $";
 var ALTUI_registered = null;
 var NULL_DEVICE = "0-0";
 var NULL_SCENE = "0-0";
@@ -5275,7 +5275,7 @@ var UIManager  = ( function( window, undefined ) {
 		var dialog = DialogManager.registerDialog('dialogModal',
 						defaultDialogModalTemplate.format( 'dialogModal',
 						'Image Properties',																// title
-						"<form>"+propertyline.format( widget.properties.url.htmlEncode() )+"</form>",	// body
+						propertyline.format( widget.properties.url.htmlEncode() ),	// body
 						"modal-lg",	// size
 						""	// glyph icon
 					));
@@ -11681,7 +11681,7 @@ var UIManager  = ( function( window, undefined ) {
 				var dialog = DialogManager.registerDialog('dialogModal',
 								defaultDialogModalTemplate.format('dialogModal',
 								_T('Command Parameters'),		// title
-								"<form></form>",				// body
+								"",				// body
 								"modal-lg",	// size
 								""	// glyph icon
 							));
