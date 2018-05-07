f99e84d515b939e26b19cae28d11dcc20b02e943
amg0 <alexis.mermet@gmail.com>
2018-05-08 00:05:00 +0200

    NETMON device support

--- L_ALTUI.lua
+++ L_ALTUI.lua
@@ -9,7 +9,7 @@
 local MSG_CLASS = "ALTUI"
 local ALTUI_SERVICE = "urn:upnp-org:serviceId:altui1"
 local devicetype = "urn:schemas-upnp-org:device:altui:1"
-local version = "v2.24"
+local version = "v2.25"
 local SWVERSION = "3.3.1"	-- "2.2.4"
 local UI7_JSON_FILE= "D_ALTUI_UI7.json"
 local ALTUI_SONOS_MP3 = "altui-sonos.mp3"
@@ -3002,6 +3002,10 @@ local function getDefaultConfig()
 	tbl["urn:schemas-upnp-org:device:althue:1"]= {
 		["ScriptFile"]="J_ALTUI_iphone.js",
 		["DeviceDrawFunc"]="ALTUI_IPhoneLocator.drawALTHue",
+	}
+	tbl["urn:schemas-upnp-org:device:netmon:1"]= {
+		["ScriptFile"]="J_ALTUI_iphone.js",
+		["DeviceDrawFunc"]="ALTUI_IPhoneLocator.drawNETMON",
 	}
 	tbl["urn:schemas-upnp-org:device:altui:1"]= {
 		["ScriptFile"]="J_ALTUI_iphone.js",
