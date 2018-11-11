68f0e7722d9a17940472ad5a5f79cd2667ee692b
alexis mermet <alexis.mermet@gmail.com>
2018-11-11 23:34:05 +0100

    test

--- J_ALTUI_uimgr.js
+++ J_ALTUI_uimgr.js
@@ -8,7 +8,7 @@
 // written devagreement from amg0 / alexis . mermet @ gmail . com
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 /*The MIT License (MIT)
 BOOTGRID: Copyright (c) 2014-2015 Rafael J. Staib
