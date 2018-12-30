5c23c2053aafa29633e8bd80724c410010969b42
alexis mermet <alexis.mermet@gmail.com>
2018-12-31 00:29:26 +0100

    avoid forcing a volume setting on TTS action

--- L_ALTUI.lua
+++ L_ALTUI.lua
@@ -9,7 +9,7 @@
 local MSG_CLASS = "ALTUI"
 local ALTUI_SERVICE = "urn:upnp-org:serviceId:altui1"
 local devicetype = "urn:schemas-upnp-org:device:altui:1"
-local version = "v2.37"
+local version = "v2.38"
 local SWVERSION = "3.3.1"	-- "2.2.4"
 local UI7_JSON_FILE= "D_ALTUI_UI7.json"
 local ALTUI_SONOS_MP3 = "altui-sonos.mp3"
@@ -3552,7 +3552,7 @@ end
 function sayTTS(lul_device,newMessage,volume,groupDevices)
 	lul_device = tonumber(lul_device)
 	newMessage = modurl.escape( newMessage or "" )
-	volume = tonumber(volume or 30)
+	volume = volume or 0
 	groupDevices = groupDevices or ""
 	log(string.format("sayTTS(%d,%s,%d,%s)",lul_device, newMessage,volume,groupDevices))
 
